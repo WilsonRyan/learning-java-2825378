f8fd2bdd47e863d812bf87cda0c9a0280f666927
Ryan Wilson <ryan.wilson96@gmail.com>
2024-04-05 14:37:37 +0900

    20240405 - 1436

---
+++ src/Main.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+//        int studentAge = 15;
+//        double studentGPA = 3.45;
+//        String studentFirstName = "Ryan";
+//        String studentLastName = "Wilson";
+//        String studentFullName = studentFirstName + " " + studentLastName;
+//        char studentFirstInitial = studentFirstName.charAt(0);
+//        char studentLastInitial = studentLastName.charAt(0);
+//        boolean hasPerfectAttendance = true;
+//
+//        System.out.println(studentFirstName + " " + studentLastName + " has a GPA of " + studentGPA);
+//        System.out.println("What do you want to update it to?");
+//
+//        Scanner input = new Scanner(System.in);
+//        studentGPA = input.nextDouble();
+//
+//  --------------------------------------------------------------------------------------------
+//
+//        System.out.println("Thanks! " + studentFirstName + " " + studentLastName + " has a new GPA of " + studentGPA + ".");
+//        Scanner input = new Scanner(System.in);
+//        boolean isOnRepeat = true;
+//        while(isOnRepeat) {
+//            System.out.println("Playing current song");
+//            System.out.println("Would you like to take this song off of repeat? If so, answer yes");
+//            String userInput = input.next();
+//
+//            if(userInput.equals("yes")) {
+//                isOnRepeat = false;
+//            }
+//        }
+//        System.out.println("Playing next song.");
+//
+//  --------------------------------------------------------------------------------------------
+//
+//        String question = "What is the combo for the 500KG bomb in Helldivers 2?";
+//        String choice1 = "Up, Right, Down, Down, Right";
+//        String choice2 = "Up, Right, Down, Down, Down"; // THIS IS THE CORRECT ONE!!
+//        String choice3 = "Up, Down, Right, Right, Down";
+//
+//        System.out.println(question);
+//        System.out.println("A - " + choice1);
+//        System.out.println("B - " + choice2);
+//        System.out.println("C - " + choice3);
+//        boolean inQuiz = true;
+//        while (inQuiz) {
+//            Scanner input = new Scanner(System.in);
+//            String userInput = input.next();
+//            userInput = userInput.toUpperCase();
+//            if (userInput.equals("B")) {
+//                System.out.println("YOU ARE CORRECT!");
+//                inQuiz = false;
+//            } else if (userInput.equals("A")) {
+//                System.out.println("You are incorrect. The correct answer is B.");
+//                inQuiz = false;
+//            } else if (userInput.equals("C")) {
+//                System.out.println("You are incorrect. The correct answer is B.");
+//                inQuiz = false;
+//            } else {
+//                System.out.println("Please input a valid input");
+//            }
+//        }
+//
+//  --------------------------------------------------------------------------------------------
+//
+
+    }
+}
